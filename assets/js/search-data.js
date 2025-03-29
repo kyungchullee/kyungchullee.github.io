66afa9b99f9d4ee14bc9c150d0946e5a1117cbba
kyungchullee <46557281+kyungchullee@users.noreply.github.com>
2025-03-29 11:46:29 +0000

    Deploying to gh-pages from @ kyungchullee/kyungchullee.github.io@773c7d2948c041780e6a28a774b49a99629827e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/research/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-photos",
           title: "photos",
